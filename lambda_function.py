242fec2a262214246a1d15c6d35eef37c5a8294a
saraspatil <97668958+saraspatil@users.noreply.github.com>
2022-01-17 14:37:04 +0530

    upload file from pycharm

---
+++ lambda_function.py
@@ -0,0 +1,9 @@
+'''
+WA lambda function:
+    - to get a cube of a number
+    - to find out even number
+    - to find out even and odd number
+    - to calculate average of 3 numbers
+    - calculate length of a given string
+    - to find out either the 2 numbers equal or not
+'''
